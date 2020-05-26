f8c119a8b30ec6b34cff72f343b2ce91e80a29b4
Ricky Thomson <flytheflag@gmx.co.uk>
2020-05-26 21:23:04 +0100

    port to python3

---
+++ src/generator.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+import random
+import json
+import re
+
+with open('sentences', 'r') as rfile:
+  sentences=rfile.readlines()
+
+with open('vocabulary.json', 'r') as rfile:
+  vocabulary=json.load(rfile)
+
+def parse_sentence(sentence):
+  for token in re.findall("\[(.*?)\]", sentence):
+    parts = token.split("-")
+    type = parts[1]
+
+    if parts[0] == "verb":
+      items = list(vocabulary["verbs"][type])
+
+      if parts[2] == "root":
+        sentence = re.sub("(\[" + token + "\])", random.choice(items)[0], sentence)
+        continue
+      elif parts[2] == "past":
+        sentence = re.sub("(\[" + token + "\])", random.choice(items)[1], sentence)
+        continue
+      elif parts[2] == "present":
+        sentence = re.sub("(\[" + token + "\])", random.choice(items)[2], sentence)
+        continue
+
+    if parts[0] == "noun":
+      items = list(vocabulary["nouns"][type])
+
+      sentence = re.sub("(\[" + token + "\])", random.choice(items), sentence)
+      continue
+    elif parts[0] == "simile":
+      items = list(vocabulary["similes"][type])
+      sentence = re.sub("(\[" + token + "\])", random.choice(items), sentence)
+      continue
+
+  return sentence
+
+def generate(num):
+  output = ""
+  for i in range(num):
+    sentence = random.choice(sentences)
+    output = output + " " + (parse_sentence(sentence.rstrip()))
+
+  return output
+
+
+print (generate(5))
+
+#def parse_sentence():
+
+
+
+
